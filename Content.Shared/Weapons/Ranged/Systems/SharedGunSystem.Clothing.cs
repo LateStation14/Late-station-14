a1385ad41b11dd0b114be785118d527abd2f2dcd
DoggowithaKeyboard <gaincarlo48@gmail.com>
2025-07-27 11:45:48 -0700

    AtmosFirefighterTank&Files

--- Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Clothing.cs
+++ Content.Shared/Weapons/Ranged/Systems/SharedGunSystem.Clothing.cs
@@ -1,11 +1,30 @@
-using Content.Shared.Containers;
+// SPDX-FileCopyrightText: 2023 Leon Friedrich <60421075+ElectroJr@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2023 Nemanja <98561806+EmoGarbage404@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2023 TaralGit <76408146+TaralGit@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2023 and_a <and_a@DESKTOP-RJENGIR>
+// SPDX-FileCopyrightText: 2023 themias <89101928+themias@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2024 Aiden <aiden@djkraz.com>
+// SPDX-FileCopyrightText: 2024 Plykiya <58439124+Plykiya@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2024 slarticodefast <161409025+slarticodefast@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 Steve <marlumpy@gmail.com>
+// SPDX-FileCopyrightText: 2025 marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
+// SPDX-FileCopyrightText: 2025 taydeo <td12233a@gmail.com>
+//
+// SPDX-License-Identifier: MIT
+
+using System.Diagnostics.CodeAnalysis;
+using Content.Shared.Inventory;
 using Content.Shared.Weapons.Ranged.Components;
 using Content.Shared.Weapons.Ranged.Events;
+using Content.Shared.Hands.EntitySystems;
 
 namespace Content.Shared.Weapons.Ranged.Systems;
 
 public partial class SharedGunSystem
 {
+    [Dependency] private readonly InventorySystem _inventory = default!;
+//    [Dependency] private readonly SharedHandsSystem _handsSystem = default!;
+
     private void InitializeClothing()
     {
         SubscribeLocalEvent<ClothingSlotAmmoProviderComponent, TakeAmmoEvent>(OnClothingTakeAmmo);
@@ -14,21 +33,55 @@ public partial class SharedGunSystem
 
     private void OnClothingTakeAmmo(EntityUid uid, ClothingSlotAmmoProviderComponent component, TakeAmmoEvent args)
     {
-        var getConnectedContainerEvent = new GetConnectedContainerEvent();
-        RaiseLocalEvent(uid, ref getConnectedContainerEvent);
-        if(!getConnectedContainerEvent.ContainerEntity.HasValue)
+        if (!TryGetClothingSlotEntity(uid, component, out var entity))
             return;
-
-        RaiseLocalEvent(getConnectedContainerEvent.ContainerEntity.Value, args);
+        RaiseLocalEvent(entity.Value, args);
     }
 
     private void OnClothingAmmoCount(EntityUid uid, ClothingSlotAmmoProviderComponent component, ref GetAmmoCountEvent args)
     {
-        var getConnectedContainerEvent = new GetConnectedContainerEvent();
-        RaiseLocalEvent(uid, ref getConnectedContainerEvent);
-        if (!getConnectedContainerEvent.ContainerEntity.HasValue)
+        if (!TryGetClothingSlotEntity(uid, component, out var entity))
             return;
+        RaiseLocalEvent(entity.Value, ref args);
+    }
 
-        RaiseLocalEvent(getConnectedContainerEvent.ContainerEntity.Value, ref args);
+    private bool TryGetClothingSlotEntity(EntityUid uid, ClothingSlotAmmoProviderComponent component, [NotNullWhen(true)] out EntityUid? slotEntity)
+    {
+        slotEntity = null;
+
+        if (!Containers.TryGetContainingContainer((uid, null, null), out var container))
+            return false;
+        var user = container.Owner;
+
+        // Assmos extinguisher nozzle changes start here
+        if (component.CheckHands)
+        {
+            foreach (var item in _hands.EnumerateHeld(user))
+            {
+                if (item == uid)
+                    continue;
+
+                if (!_whitelistSystem.IsWhitelistFailOrNull(component.ProviderWhitelist, item))
+                {
+                    slotEntity = item;
+                    return true;
+                }
+            }
+        }
+        // Assmos changes end
+
+        if (!_inventory.TryGetContainerSlotEnumerator(user, out var enumerator, component.TargetSlot))
+            return false;
+
+        while (enumerator.NextItem(out var item))
+        {
+            if (_whitelistSystem.IsWhitelistFailOrNull(component.ProviderWhitelist, item))
+                continue;
+
+            slotEntity = item;
+            return true;
+        }
+
+        return false;
     }
 }
